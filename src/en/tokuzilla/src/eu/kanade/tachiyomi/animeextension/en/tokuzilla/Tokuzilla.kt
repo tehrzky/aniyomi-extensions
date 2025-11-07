bba1f3c9290844644b9efcd0d2a0fe550ccbadc6
tehrzky <61300808+tehrzky@users.noreply.github.com>
2025-11-07 17:43:20 +0800

    Update Tokuzilla.kt

--- src/en/tokuzilla/src/eu/kanade/tachiyomi/animeextension/en/tokuzilla/Tokuzilla.kt
+++ src/en/tokuzilla/src/eu/kanade/tachiyomi/animeextension/en/tokuzilla/Tokuzilla.kt
@@ -7,8 +7,6 @@ import eu.kanade.tachiyomi.animesource.model.Video
 import eu.kanade.tachiyomi.animesource.online.ParsedAnimeHttpSource
 import eu.kanade.tachiyomi.network.GET
 import eu.kanade.tachiyomi.util.asJsoup
-import okhttp3.Headers
-import okhttp3.OkHttpClient
 import okhttp3.Request
 import okhttp3.Response
 import org.jsoup.nodes.Document
